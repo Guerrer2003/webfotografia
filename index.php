bf2e0abaf3d13bff0d3723020f26d8544199d3da
Guerrer2003 <guerrerojj937@gmail.com>
2024-03-07 01:35:12 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php include("cabecera.php"); ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+
+<div class="container">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="https://marcosllanos.com/media/Sesion-de-conexion-por-Marcos-Llanos.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://marcosllanos.com/media/wedding-on-the-mountain-Mendoza.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://marcosllanos.com/media/winery-wedding-in-Mendoza-Photographer-de-casamento.jpg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+<!-- Agrega el enlace al archivo JavaScript de Bootstrap y a jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+
+
+<h2>Quienes somos?</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi cum incidunt beatae dicta impedit, quidem explicabo quia ipsum exercitationem pariatur laboriosam id debitis sunt inventore necessitatibus totam amet. Officia, alias.</p>
+
+
+
+
+
+
+
+</body>
+</html>
+
+<?php include("pie.php"); ?>
+
+
+
